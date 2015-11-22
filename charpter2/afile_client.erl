4bc347151cbde4f6a0e9f061fafc6f961b99a829
bigbully <375795132@qq.com>
2015-11-22 20:54:26 +0800

    第二章

---
+++ charpter2/afile_client.erl
@@ -0,0 +1,24 @@
+-module(afile_client).
+-export([ls/1, get_file/2, put_file/1]).
+
+ls(Server) ->
+	Server ! {self(), list_dir},
+	receive
+		{Server, FileList} ->
+			FileList
+	end.
+
+
+get_file(Server, File) -> 
+	Server ! {self(), {get_file, File}},
+	receive 
+		{Server, Content} -> 
+			Content
+	end.
+
+put_file(Server) ->
+	Server ! {self(), {put_file, "newName"}},
+	receive 
+		{Server, FileList} ->
+			FileList
+	end.
--- charpter2/afile_server.erl
+++ charpter2/afile_server.erl
@@ -9,6 +9,10 @@ loop(Dir) ->
 			Client ! {self(), file:list_dir(Dir)};
 		{Client, {get_file, File}} ->
 			Full = filename:join(Dir, File),
-			Client ! {self(), file:read_file(Full)}
+			Client ! {self(), file:read_file(Full)};
+		{Client, {put_file, Name}} ->
+			FullName = filename:join(Dir, Name),
+			file:write_file(FullName, Name),
+			Client ! {self(), file:list_dir(Dir)}
 	end,
 	loop(Dir).
