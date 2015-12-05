79de220e2d74643e887d4647570c1b84ac2483bc
晋助 <biandi.bd@alibaba-inc.com>
2015-12-05 21:30:19 +0800

    练习题

--- chapter4/geometry.erl
+++ chapter4/geometry.erl
@@ -1,8 +1,15 @@
 -module(geometry).
--export([area/1, test/0]).
+-export([area/1, test/0, girth/1]).
 
 area({rectangle, Width, Height}) -> Width * Height;
-area({square, Side}) -> Side * Side.
+area({square, Side}) -> Side * Side;
+area({circle, R}) -> 3.14 * R * R;
+area({triangle, A, B}) -> A * B / 2.
+
+girth({rectangle, Width, Height}) -> (Width + Height) * 2;
+girth({square, Side}) -> Side * 4;
+girth({circle, R}) ->  2 * 3.14 * R;
+girth({triangle, A, B}) -> math:sqrt(A * A + B * B).
 
 test() ->
 	12 = area({rectangle, 3, 4}),
